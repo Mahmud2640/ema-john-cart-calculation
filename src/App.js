aaa42040f71273c764c5895c6a8da78e49befb6d
Mahmud2640 <mdmahmud484@gmail.com>
2022-04-02 14:40:37 +0600

    adeed some components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
-import Header from './components/Header/Header';
-import Shop from './components/Shop/Shop';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Shop from "./components/Shop/Shop";
+import Orders from "./components/Orders/Orders"
+import Inventory from "./components/Inventory/Inventory"
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Routes>
+        <Route path="/" element={<Shop></Shop>}></Route>
+        <Route path="/home" element={<Shop></Shop>}></Route>
+        <Route path="/orders" element={<Orders></Orders>}></Route>
+        <Route path="/inventory" element={<Inventory></Inventory>}></Route>
+      </Routes>
       <Shop></Shop>
     </div>
   );
